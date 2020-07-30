a1140752c0bfd7c2717955e285e57bafc68f4de8
Zlatika <zlata@ellery.ru>
2020-07-30 18:15:11 +0300

    error fixed. firehook&showing images started

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
+// import { motion } from 'framer-motion';
 
-const ProgressBar = (file, setFile) => {
+const ProgressBar = ({ file, setFile }) => {
+  const { progress, url } = useStorage(file);
+  console.log(progress, url)
 
-  const { url, progress } = useStorage(file);
-console.log(progress, url)
+
+
+  useEffect(() => {
+    if(url){
+      setFile(null)
+    }
+   }, [url, setFile]);
+
 
   return (
-    <div className="progress-bar">
-
+    <div className="progress-bar" style={{ width: progress + '%' }}>
+     
     </div>
   )
 }
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { projectStorage } from '../firebase/config'
+import { projectStorage, projectFirestore, timestamp } from '../firebase/config'
 
 const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
@@ -11,6 +11,7 @@ const useStorage = (file) => {
   useEffect(() => {
     //REFERENCES
     const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFirestore.collection('images');
 
     storageRef.put(file).on('state_changed', (snap) => {
       let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
@@ -19,7 +20,9 @@ const useStorage = (file) => {
       setError(err);
     }, async () => {
       const url = await storageRef.getDownloadURL();
-      setUrl(url)
+      const createdAt = timestamp();
+      collectionRef.add({url, createdAt})
+      setUrl(url);
     })
   }, [file]);
 
